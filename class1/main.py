9941c875fded350720d4a4be1e2ae7d35d5c5100
MIM432 <m.ibrahim135902@gmail.com>
2025-05-05 16:41:26 +0500

    Add files via upload

---
+++ class1/main.py
@@ -0,0 +1,47 @@
+# name:str = 'Governor Intiaitive for AI and Computing'
+# print("GIAIC stand for :", name)
+# print("Hello World Anas!")
+# def giag():
+#     print("Hello World2!")
+
+# Assigment 
+
+# > X par account banana hai or aj ki class ka overview hai
+# > colab ko parna hai
+# text:str ="Hello World"
+# print (text)
+
+# str : string
+
+id_card:str = """Name:Anas 
+Father Name:Tariq Ali
+Age:20
+"""
+# print(id_card)
+
+# int : Number
+
+num :int = 35
+# print(num)
+# float
+float_num : float = 2.33
+# print (float_num)
+# bolean
+is_ture :bool = True
+is_false:bool =False
+# print (bool)
+# List
+fruit:list[str] =["apple","cherry","mango"]
+# print(fruit[0])
+# print(fruit[1])
+# Neagetive Indexing
+# print(fruit[-1])
+
+# tuple
+tuple_data:tuple = ("apple","cherry")
+
+dict_data:dict[str,str] = {
+    "Name:Anas",
+    "FatherName:Tariq ALi"
+}
+print (dict_data)
